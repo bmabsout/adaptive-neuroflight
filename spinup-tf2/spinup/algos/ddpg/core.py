5cf14cb588af37e73c5d9b84a89e9ed2b44519f0
Neuroflight <Neuroflight>
2022-02-06 12:19:59 -0500

    removed bias because the agent should be centered around 0

--- spinup-tf2/spinup/algos/ddpg/core.py
+++ spinup-tf2/spinup/algos/ddpg/core.py
@@ -2,17 +2,16 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import regularizers
 
-def mlp_functional(input_shape, hidden_sizes=(32,), activation='tanh', output_activation=None):
+def mlp_functional(input_shape, hidden_sizes=(32,), activation='tanh', output_activation=None, use_bias=True):
     inputs = tf.keras.Input(input_shape)
     layer=inputs
     for hidden_size in hidden_sizes[:-1]:
-        layer = tf.keras.layers.Dense(units=hidden_size, activation=activation, kernel_regularizer=regularizers.l1_l2(l1=1e-4, l2=1e-3))(layer)
+        layer = tf.keras.layers.Dense(units=hidden_size, activation=activation, use_bias=use_bias)(layer)
     outputs = tf.keras.layers.Dense(
         units=hidden_sizes[-1],
-        kernel_regularizer=regularizers.l1_l2(l1=1e-4, l2=1e-3),
         activation=output_activation,
-        bias_regularizer=regularizers.l2(1e-3),
-        activity_regularizer=regularizers.l2(5e-3)
+        use_bias=use_bias,
+        activity_regularizer=regularizers.l2(1e-3)
     )(layer)
     return tf.keras.Model(inputs=inputs, outputs=outputs)
 
@@ -32,7 +31,7 @@ Actor-Critics
 def mlp_actor_critic(obs_dim, act_dim, hidden_sizes=(64,64), activation='tanh', 
                      output_activation='tanh'):
     with tf.name_scope('pi'):
-        pi_network = mlp_functional((obs_dim,), list((64,64))+[act_dim], activation, output_activation)
+        pi_network = mlp_functional((obs_dim,), list((32,32))+[act_dim], activation, output_activation, use_bias=False)
     with tf.name_scope('q'):
         q_network = mlp_functional((obs_dim+act_dim,), list(hidden_sizes)+[1], activation, None)
     return pi_network, q_network
